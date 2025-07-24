7489d09cd2a02c4aeedd6fced97f42f7fdb4cab1
Amin <amin.albooyeh@gmail.com>
2025-07-24 19:35:10 +0200

    improving UI

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from kivy.app import App
 import configparser
 import threading
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.logger import Logger
 from kivy.clock import mainthread
@@ -13,13 +13,8 @@ Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '300')
 
 
-class WeatherLayout(BoxLayout):
-    city_input = ObjectProperty(None)
-    weather_info_label = ObjectProperty(None)
-    weather_icon = ObjectProperty(None)
-    results_layout = ObjectProperty(None)
-    loading_spinner = ObjectProperty(None)
-
+class WeatherLayout(FloatLayout):
+    
     def search_weather(self):
         city_name = self.ids.city_input.text.strip()
         if not city_name:
@@ -32,6 +27,22 @@ class WeatherLayout(BoxLayout):
         # Run the network request in a separate thread to avoid freezing the UI
         thread = threading.Thread(target=self._fetch_weather_data, args=(city_name,))
         thread.start()
+
+    def _get_background_image(self, weather_condition):
+        """Returns the path to a background image based on the weather."""
+        condition = weather_condition.lower()
+        if 'clear' in condition:
+            return 'images/clear.jpg'
+        elif 'clouds' in condition:
+            return 'images/clouds.jpg'
+        elif 'rain' in condition or 'drizzle' in condition:
+            return 'images/rain.jpg'
+        elif 'snow' in condition:
+            return 'images/snow.jpg'
+        elif 'thunderstorm' in condition:
+            return 'images/thunderstorm.jpg'
+        # Default background for other conditions like mist, fog, etc.
+        return 'images/default.jpg'
 
     def show_loading(self, is_loading):
         self.ids.results_layout.opacity = 0 if is_loading else 1
@@ -78,12 +89,18 @@ class WeatherLayout(BoxLayout):
             humidity = main_data.get('humidity', 'N/A')
 
             weather_list = weather_data.get('weather', [])
+            weather_main_condition = ''
             if weather_list:
                 description = weather_list[0].get('description', 'No description')
                 icon_code = weather_list[0].get('icon', '')
+                # Get the main weather condition (e.g., 'Clouds', 'Clear', 'Rain')
+                weather_main_condition = weather_list[0].get('main', '')
             else:
                 description = 'No description'
                 icon_code = ''
+
+            # Set the background image based on the weather condition
+            self.ids.background_image.source = self._get_background_image(weather_main_condition)
 
             if icon_code:
                 self.ids.weather_icon.source = f"http://openweathermap.org/img/wn/{icon_code}@2x.png"
@@ -105,6 +122,7 @@ class WeatherLayout(BoxLayout):
     def _update_ui_on_error(self, message):
         self.ids.weather_info_label.text = message
         self.ids.weather_icon.source = ''
+        self.ids.background_image.source = 'images/default.jpg'
         self.show_loading(False)
 
 
